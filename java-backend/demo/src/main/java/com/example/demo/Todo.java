156f7404597e1eda9ac624fc3e02e5d7ec204540
Yuuske Nakajima <yuu.rzsui@gmail.com>
2025-06-30 19:28:58 +0900

    バックエンド　基本的な機能

--- java-backend/demo/src/main/java/com/example/demo/Todo.java
+++ java-backend/demo/src/main/java/com/example/demo/Todo.java
@@ -3,10 +3,12 @@ package com.example.demo;
 public class Todo {
     private int id;
     private String task;
+    private boolean done;
 
-    public Todo(int id, String task){
+    public Todo(int id, String task, boolean done) {
         this.id = id;
         this.task = task;
+        this.done = done;
     }
 
     public int getId(){
@@ -15,5 +17,21 @@ public class Todo {
 
     public String getTask(){
         return task;
+    }
+
+    public boolean isDone(){
+        return done;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
     }
 }
--- java-backend/demo/src/main/java/com/example/demo/TodoController.java
+++ java-backend/demo/src/main/java/com/example/demo/TodoController.java
@@ -4,16 +4,47 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/todos")
 public class TodoController {
 
-    private List<Todo> todos = new ArrayList<>();
+    private List<Todo> todos = new ArrayList<>(List.of(
+        new Todo(1,"牛乳を買う",false),
+        new Todo(2,"卵を買う",true)
+    ));
 
-    @GetMapping("/todos")
+    @GetMapping
     public List<Todo> getTodos() {
-        return List.of(
-            new Todo(1, "買い物に行く"),
-            new Todo(2, "宿題をやる"),
-            new Todo(3, "運動する")
-        );
+        return todos;
     }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo newtodo) {
+        todos.add(newtodo);
+        return newtodo;
+    }
+
+    @DeleteMapping("/{todoId}")
+    public void deleteTodo(@PathVariable int todoId) {
+        todos.removeIf(todo -> todo.getId() == todoId);
+    }
+    @PutMapping("/{todoId}")
+    public Todo updateTodo(@PathVariable int todoId, @RequestBody Todo updatedTodo) {
+        for(int i = 0; i < todos.size(); i++) {
+            if (todos.get(i).getId() == todoId) {
+                todos.set(i, updatedTodo);
+                return updatedTodo;
+            }
+        }
+        throw new NoSuchElementException("指定されたIDのタスクが見つかりません: " + todoId);
+    }
+    @GetMapping("/{todoId}")
+    public Todo getTodoById(@PathVariable int todoId) {
+        for (Todo todo : todos) {
+            if (todo.getId() == todoId) {
+                return todo;
+            }
+        }
+        throw new NoSuchElementException("指定されたIDのタスクが見つかりません: " + todoId);
+    }
+    
 }
